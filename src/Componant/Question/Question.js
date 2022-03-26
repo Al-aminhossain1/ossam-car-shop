7094033e7c19682a548d4f8e2004027690149f2d
Al-aminhossai1 <md.al.aminhossain1785@gmail.com>
2022-03-26 22:02:18 +0600

    solved

--- src/Componant/Question/Question.js
+++ src/Componant/Question/Question.js
@@ -5,8 +5,8 @@ const Question = () => {
         <div>
             <h3>Question 01: State VS Props
             </h3>
-            <p>STATE:  The state of the website is changeable and may change over time. And used to hold data. When a user visits a website, he can change the status if he wants, but not again. <br /><br />
-                PROPS::We pass as props to pass data to a child component through a component. Props remain unchanged Props can be read. Its value cannot be changed. As props we can send data inside hardcodes, we can send data dynamically and we can also send functions.</p>
+            <p>State:  The state of the website is changeable and may change over time. And used to hold data. When a user visits a website, he can change the status if he wants, but not again. <br /><br />
+                Props:We pass as props to pass data to a child component through a component. Props remain unchanged Props can be read. Its value cannot be changed. As props we can send data inside hardcodes, we can send data dynamically and we can also send functions.</p>
             <h3>Question 02: Usestate</h3>
             <p>Usetate is one of the hooks in react. Through which the functional component is placed in the state variable. Provides a variable with a function to determine the value of the current state by changing the value of the initial state with the Usestate.</p>
         </div>
